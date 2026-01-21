7ac12c8cc83ff2a0d22e83c989a0526aa011d3d5
Thomas GUERY <originaltompouce@gmail.com>
2026-01-21 12:23:26 +0100

    filter all messages by slot name

--- src/main.cpp
+++ src/main.cpp
@@ -280,7 +280,9 @@ void connect_ap(std::string uri="")
             printf("%s\n", msg.c_str());
         });
     ap->set_print_json_handler([](const std::list<APClient::TextNode>& msg) {
-            //printf("%s\n", ap->render_json(msg, APClient::RenderFormat::ANSI).c_str());
+        std::string tes = ap->render_json(msg, APClient::RenderFormat::ANSI).c_str();
+        if (game && tes.find(game->get_slot()) != std::string::npos)
+            printf("%s\n", tes);
         });
     ap->set_bounced_handler([](const json& cmd) {
         if (game->want_deathlink()) {
